b4dc83413cc128ab587adf90be0502ff35c8c5e5
opekope2 <20487556+opekope2@users.noreply.github.com>
2025-08-03 21:54:30 +0200

    Update logging

--- contents/src/workspace/manager/SharedWorkspaceManager.ts
+++ contents/src/workspace/manager/SharedWorkspaceManager.ts
@@ -17,24 +17,21 @@ export class SharedWorkspaceManager extends WorkspaceManager {
     currentWorkspace.deactivate();
     newWorkspace.activate(output);
     this.activeWorkspace.set(output, newWorkspace);
-    print(`[PMW] Activate workspace ${ws + 1} output ${output.name}`);
+    print(`[PMW] Activate workspace ${ws + 1}, output ${output.name}`);
   }
 
   moveToWorkspace(window: Window, ws: number): void {
     if (ws < 0 || ws >= this.workspaces.length) return;
     ref(window).workspace = this.workspaces[ws];
-    print(`[PMW] Move to workspace ${window} [${window.caption}] ${ws + 1}`);
+    print(`[PMW] Move to workspace ${ws + 1}, window [${window.caption}]`);
   }
 
   protected addOutput(output: Output): void {
     const freeWorkspaces = this.workspaces.filter((it) => !it.active);
     freeWorkspaces[0].activate(output);
     this.activeWorkspace.set(output, freeWorkspaces[0]); // TODO not enough workspaces
-    print(
-      `[PMW] Add output ${output.name} workspace ${
-        freeWorkspaces[0].ordinal + 1
-      }`
-    );
+    const ws = freeWorkspaces[0].ordinal;
+    print(`[PMW] Add output ${output.name} workspace ${ws + 1}`);
   }
 
   protected removeOutput(output: Output): void {
